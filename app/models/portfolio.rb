51882d0fcf54954598213907ec1b3b255502f007
Dani Gunawan <danigunawan.elektroug@gmail.com>
2017-12-31 22:18:32 +0700

    implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 	
 	def self.angular
@@ -10,16 +11,22 @@ class Portfolio < ApplicationRecord
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||= "http://placehold.it/600x400" 
+		
+		## Jika menggunakan template concerns placeholder atau cara normalnya 
+		#self.main_image ||= "http://placehold.it/600x400" 
 		# diatas ||= ini sama dengan if else yang dibawah
 			# if self.main_image == nil
 			# 	self.main_image = "http://placehold.it/600x400"
 			# end
-		self.thumb_image ||= "http://placehold.it/350x200"
+		#self.thumb_image ||= "http://placehold.it/350x200"
 			# diatas ||= ini sama dengan if else yang dibawah
 			# if self.thumb_image == nil
 			# 	self.thumb_image = "http://placehold.it/350x200"
 			# end
+
+		## Jika menggunakan cara concerns placeholder templatenya
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 	end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171225192611) do
+ActiveRecord::Schema.define(version: 20171231145257) do
 
   create_table "blogs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20171225192611) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
