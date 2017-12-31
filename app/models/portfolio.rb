6cdce336054fa6f6065ffc873dcccf1c54038631
Dani Gunawan <danigunawan.elektroug@gmail.com>
2017-12-31 21:45:54 +0700

    implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,23 @@ class Portfolio < ApplicationRecord
 
 	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby On Rails') }
 
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400" 
+		# diatas ||= ini sama dengan if else yang dibawah
+			# if self.main_image == nil
+			# 	self.main_image = "http://placehold.it/600x400"
+			# end
+		self.thumb_image ||= "http://placehold.it/350x200"
+			# diatas ||= ini sama dengan if else yang dibawah
+			# if self.thumb_image == nil
+			# 	self.thumb_image = "http://placehold.it/350x200"
+			# end
+	end
+
+
+
+	
+
 end
