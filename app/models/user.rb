a4ded9d6abb7d48aab77aa0c8c0a173060830ef3
Dani Gunawan <danigunawan.elektroug@gmail.com>
2018-01-21 12:16:27 +0700

    Completed Authentication with virtual attributes for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # fungsi validasi
+  validates_presence_of :name
+
+  # fungsi ekstraksi first and last name data
+  def first_name
+  	# self.name.split[0] atau
+  	self.name.split.first
+  end
+
+  def last_name
+  	# self.name.split[1] atau
+  	self.name.split.last
+  end
 end
