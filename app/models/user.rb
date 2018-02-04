3451ce71c646e0654fe67bb4785fb0b2cf3719ec
Dani Gunawan <danigunawan.elektroug@gmail.com>
2018-02-04 15:46:43 +0700

    Implemented error management rails application part 1-2 108-107

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,25 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
   # fungsi validasi
+  #validates_presence_of :name # jika ingin coba menggunakan rescue
   validates_presence_of :name
 
   # fungsi ekstraksi first and last name data
+
+  # # Fungsi dengan rescue (jika digunakan maka yang tadinya error karena method oops tidak ada jadi di ignore kan dan error hilang persis seperti error_reporting pada php)
+  # def first_name
+  # 	begin
+  # 	# self.name.split[0] atau
+  # 	self.name.split.first.oops
+  # rescue
+  # end
+  # end
+
+   # Fungsi dengan rescue
   def first_name
-  	# self.name.split[0] atau
+   # self.name.split[0] atau
   	self.name.split.first
   end
 
@@ -16,4 +28,14 @@ class User < ApplicationRecord
   	# self.name.split[1] atau
   	self.name.split.last
   end
+
+  # Fungsi cobacoba aja pada part 108
+
+  # def get_facebook_messages
+  # 	contacts_fb.oops
+  # 	@messages = retrieves_messages
+  # rescue IOError => e
+  # 	flash[:error] = "Error occured contacting Facebook: #{e}"
+  # end
+
 end
