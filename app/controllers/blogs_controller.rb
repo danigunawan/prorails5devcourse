8c0692362ce46e49877977bc2583b029dd05864d
Dani Gunawan <danigunawan.elektroug@gmail.com>
2018-05-10 21:35:18 +0700

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,9 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status] # sebelum aksi show edit update destroy memanggil call back dulu yakni set_blog
   layout "blog"
 
+  #petergate
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+
   # GET /blogs
   # GET /blogs.json
   def index
